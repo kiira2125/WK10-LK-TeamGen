465c7219e4c5381b1ff21d2582854a46e46debb8
LK <lunakiira0911@gmail.com>
2022-08-07 18:17:11 -0700

    updating

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -14,6 +14,7 @@ test("getRole() should return \"Manager\"", () => {
 });
 
 test("Can get office number via getOffice()", () => {
+  
   const testValue = 100;
   const e = new Manager("BOOBOO", 1, "luna@test.com", testValue);
   expect(e.getOfficeNumber()).toBe(testValue);
